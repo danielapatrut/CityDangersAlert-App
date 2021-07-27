88d6f842d404aab98bd4520709ad3123a0dfd344
IoanaArghisan <ioana.arghisan00@e-uvt.ro>
2021-07-27 22:13:42 +0300

    exceptions

--- app/src/main/java/com/example/citydangersalert_app/RegisterUtils.java
+++ app/src/main/java/com/example/citydangersalert_app/RegisterUtils.java
@@ -1,6 +1,7 @@
 package com.example.citydangersalert_app;
 
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.EditText;
 
 public class RegisterUtils {
@@ -31,19 +32,46 @@ public class RegisterUtils {
     }
 
     public static boolean checkEmptyEmail(EditText mEmail, String email) {
-        if(TextUtils.isEmpty(email)){
+        if(TextUtils.isEmpty(email) & !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             mEmail.setError("Email is Required!");
             return true;
         }
         return false;
+
+
     }
+
+
 
     public static boolean checkEmptyFirstName(EditText mFirstName, String firstName) {
         if(TextUtils.isEmpty(firstName)){
-            mFirstName.setError("Email is Required!");
+            mFirstName.setError("First name is Required!");
             return true;
         }
         return false;
     }
 
+    public static boolean checkEmptyLastName(EditText mLastName, String lastName) {
+        if(TextUtils.isEmpty(lastName)){
+            mLastName.setError("Last name is Required!");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkEmptyAdress(EditText mAdress, String address) {
+        if(TextUtils.isEmpty(address)){
+            mAdress.setError("Adress is Required!");
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkEmptyPhone(EditText mPhone, String phone) {
+        if(TextUtils.isEmpty(phone)){
+            mPhone.setError("Telephone number is Required!");
+            return true;
+        }
+        return false;
+    }
 }
