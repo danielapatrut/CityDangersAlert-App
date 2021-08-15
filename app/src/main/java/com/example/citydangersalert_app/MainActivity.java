fb8dffedea87d924fd7b49ddcf70d12257402ad5
IoanaArghisan <ioana.arghisan00@e-uvt.ro>
2021-08-15 20:37:38 +0300

    create logout to fix login

--- app/src/main/java/com/example/citydangersalert_app/MainActivity.java
+++ app/src/main/java/com/example/citydangersalert_app/MainActivity.java
@@ -7,20 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button mBackBtn;
+    Button mlogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBackBtn = findViewById(R.id.backButton);
+        mlogout = findViewById(R.id.backButton);
 
-        mBackBtn.setOnClickListener(new View.OnClickListener() {
+        mlogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(getApplicationContext(),LoginActivity.class));
             }
         });
